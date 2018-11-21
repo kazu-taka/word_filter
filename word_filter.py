11a6d9920ca870f4ec51f065d448993e953aca79
kazu-taka <k.takahashi@nowall.co.jp>
2018-11-21 15:30:54 +0900

    Add C-2

--- word_filter.py
+++ word_filter.py
@@ -5,11 +5,22 @@ class WordFilter:
     def detect(self, word):
         return self.word_filter in word
 
+    def censor(self, word):
+        return word.replace(self.word_filter, "<censored>")
+
 
 my_filter = WordFilter("アーセナル")
 
+# C-1
 # NGワードが含まれている場合
 print(my_filter.detect("昨日のアーセナルの試合アツかった！"))  # Trueを返す ※出力されるわけではありません！
 
 # NGワードが含まれていない場合
 print(my_filter.detect("昨日のリバプールの試合アツかった！"))  # Falseを返す ※出力されるわけではありません！
+
+# C-2
+# NGワードが含まれている場合
+print(my_filter.censor("昨日のアーセナルの試合アツかった！"))  # "昨日の<censored>の試合アツかった！" を返す ※出力されるわけではありません！
+
+# NGワードが含まれていない場合
+print(my_filter.censor("昨日のリバプールの試合アツかった！"))  # "昨日のリバプールの試合アツかった！" を返す ※出力されるわけではありません！
